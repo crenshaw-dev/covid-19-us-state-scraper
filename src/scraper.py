4c96d27dbd46b216c8a5a94b54d88bd832cdefbe
Michael Crenshaw <michael@crenshaw.dev>
2020-03-22 13:27:55 -0400

    add Arkansas, error handling

--- src/scraper.py
+++ src/scraper.py
@@ -1,3 +1,5 @@
+import sys
+
 import requests
 
 from bs4 import BeautifulSoup
@@ -10,11 +12,19 @@ state_getters = {
     'AL': {
         'url': 'http://alabamapublichealth.gov/infectiousdiseases/2019-coronavirus.html',
         'getter': lambda soup: int(soup.select_one('.mainContent > table > tbody > tr:last-child > td:last-child').text)
+    },
+    'AR': {
+        'url': 'https://www.healthy.arkansas.gov/programs-services/topics/novel-coronavirus',
+        'getter': lambda soup: int(soup.select_one('#contentBody > table:nth-of-type(4) tr:first-child > td:last-child').text)
     }
 }
 
 print('state,total_cases')
 for state, info in state_getters.items():
-    soup = BeautifulSoup(requests.get(info['url']).content, 'html5lib')
-    count = info['getter'](soup)
-    print(f'{state},{count}')
+    try:
+        soup = BeautifulSoup(requests.get(info['url']).content, 'html5lib')
+        count = info['getter'](soup)
+        print(f'{state},{count}')
+    except Exception as e:
+        print(f'{state},')
+        print(f'Failed to get stats for {state}. Error: {e}', file=sys.stderr)
