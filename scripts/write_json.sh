c1e1f49daaef5fedfda900c0ec05f916610244df
Michael Crenshaw <michael@crenshaw.dev>
2020-03-22 22:24:23 -0400

    add data and make script run on a 1hr loop

--- scripts/write_json.sh
+++ scripts/write_json.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-python ../src/scraper.py --format csv --outdir ../data
-python ../src/scraper.py --format md --outdir ../data
-python ../src/scraper.py --format json --outdir ../data
+while true
+do
+  python ../src/scraper.py --format csv --outdir ../data
+  python ../src/scraper.py --format md --outdir ../data
+  python ../src/scraper.py --format json --outdir ../data
+  sleep 1h
+done
